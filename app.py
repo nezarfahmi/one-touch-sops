8409b12ec71f5b09190e96651d2470472d5964ae
nezarfahmi <193745727+nezarfahmi@users.noreply.github.com>
2025-06-18 12:53:55 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id' : 1,
+    'name': 'Sales Engineer',
+    'location' : 'Westfield, Indiana',
+    'salary':'$100,000'
+  },
+  {
+    'id':2,
+    'name':'Electrician',
+    'location' : 'Indianapolis, Indiana',
+    'salary':'$75,000'
+  },
+  {
+    'id':2,
+    'name':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'$105,000'
+  },
+  {
+    'id':2,
+    'name':'Backend Engineer',
+    'location':'Remote',
+    'salary':'$120,000'
+  }
+]
+
 @app.route("/")
 def hello_onetouch():
-  return render_template("home.html")
+  return render_template("home.html", 
+                         jobs=JOBS, company_name='One-Touch Automation')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
